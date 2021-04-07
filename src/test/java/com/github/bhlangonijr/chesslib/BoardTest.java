ba57ff212f7d8b1b5a9d9d114e1ca5636f278d42
Ville Saalo <ville.saalo@iki.fi>
2021-04-07 15:56:50 +0300

    properly test the current Board#toString functionality

--- src/test/java/com/github/bhlangonijr/chesslib/BoardTest.java
+++ src/test/java/com/github/bhlangonijr/chesslib/BoardTest.java
@@ -661,11 +661,24 @@ public class BoardTest {
         // Creates a new chessboard in the standard initial position
         Board board = new Board();
 
-        //Make a move from E2 to E4 squares
         board.doMove(new Move(Square.E2, Square.E4));
+        board.doMove(new Move(Square.B8, Square.C6));
+        board.doMove(new Move(Square.F1, Square.C4));
 
         //print the chessboard in a human-readable form
         System.out.println(board.toString());
+
+        final String expected =
+                "r.bqkbnr\n" +
+                "pppppppp\n" +
+                "..n.....\n" +
+                "........\n" +
+                "..B.P...\n" +
+                "........\n" +
+                "PPPP.PPP\n" +
+                "RNBQK.NR\n" +
+                "Side: BLACK";
+        assertEquals(expected, board.toString());
     }
 
     @Test
