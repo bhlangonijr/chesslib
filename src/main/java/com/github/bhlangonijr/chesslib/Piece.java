f120432550d6e4ffe3bfd043f7fa412a4d9c811d
Ville Saalo <ville.saalo@iki.fi>
2021-04-20 20:28:27 +0300

    turn lookup tables in Piece enum into final fields
    This seems to have a small but measurable positive impact on performance.

--- src/main/java/com/github/bhlangonijr/chesslib/Piece.java
+++ src/main/java/com/github/bhlangonijr/chesslib/Piece.java
@@ -26,68 +26,58 @@ public enum Piece {
     /**
      * White pawn piece.
      */
-    WHITE_PAWN,
+    WHITE_PAWN(Side.WHITE, PieceType.PAWN),
     /**
      * White knight piece.
      */
-    WHITE_KNIGHT,
+    WHITE_KNIGHT(Side.WHITE, PieceType.KNIGHT),
     /**
      * White bishop piece.
      */
-    WHITE_BISHOP,
+    WHITE_BISHOP(Side.WHITE, PieceType.BISHOP),
     /**
      * White rook piece.
      */
-    WHITE_ROOK,
+    WHITE_ROOK(Side.WHITE, PieceType.ROOK),
     /**
      * White queen piece.
      */
-    WHITE_QUEEN,
+    WHITE_QUEEN(Side.WHITE, PieceType.QUEEN),
     /**
      * White king piece.
      */
-    WHITE_KING,
+    WHITE_KING(Side.WHITE, PieceType.KING),
     /**
      * Black pawn piece.
      */
-    BLACK_PAWN,
+    BLACK_PAWN(Side.BLACK, PieceType.PAWN),
     /**
      * Black knight piece.
      */
-    BLACK_KNIGHT,
+    BLACK_KNIGHT(Side.BLACK, PieceType.KNIGHT),
     /**
      * Black bishop piece.
      */
-    BLACK_BISHOP,
+    BLACK_BISHOP(Side.BLACK, PieceType.BISHOP),
     /**
      * Black rook piece.
      */
-    BLACK_ROOK,
+    BLACK_ROOK(Side.BLACK, PieceType.ROOK),
     /**
      * Black queen piece.
      */
-    BLACK_QUEEN,
+    BLACK_QUEEN(Side.BLACK, PieceType.QUEEN),
     /**
      * Black king piece.
      */
-    BLACK_KING,
+    BLACK_KING(Side.BLACK, PieceType.KING),
     /**
      * None piece.
      */
-    NONE;
+    NONE(null, null);
 
     public static Piece[] allPieces = values();
 
-    /**
-     * The Piece type.
-     */
-    static EnumMap<Piece, PieceType> pieceType =
-            new EnumMap<Piece, PieceType>(Piece.class);
-    /**
-     * The Piece side.
-     */
-    static EnumMap<Piece, Side> pieceSide =
-            new EnumMap<Piece, Side>(Piece.class);
     private static Piece[][] pieceMake = {
             {WHITE_PAWN, BLACK_PAWN},
             {WHITE_KNIGHT, BLACK_KNIGHT},
@@ -98,36 +88,12 @@ public enum Piece {
             {NONE, NONE},
     };
 
-    static {
-        pieceType.put(Piece.WHITE_PAWN, PieceType.PAWN);
-        pieceType.put(Piece.WHITE_KNIGHT, PieceType.KNIGHT);
-        pieceType.put(Piece.WHITE_BISHOP, PieceType.BISHOP);
-        pieceType.put(Piece.WHITE_ROOK, PieceType.ROOK);
-        pieceType.put(Piece.WHITE_QUEEN, PieceType.QUEEN);
-        pieceType.put(Piece.WHITE_KING, PieceType.KING);
+    private final Side side;
+    private final PieceType type;
 
-        pieceType.put(Piece.BLACK_PAWN, PieceType.PAWN);
-        pieceType.put(Piece.BLACK_KNIGHT, PieceType.KNIGHT);
-        pieceType.put(Piece.BLACK_BISHOP, PieceType.BISHOP);
-        pieceType.put(Piece.BLACK_ROOK, PieceType.ROOK);
-        pieceType.put(Piece.BLACK_QUEEN, PieceType.QUEEN);
-        pieceType.put(Piece.BLACK_KING, PieceType.KING);
-
-        pieceSide.put(Piece.WHITE_PAWN, Side.WHITE);
-        pieceSide.put(Piece.WHITE_KNIGHT, Side.WHITE);
-        pieceSide.put(Piece.WHITE_BISHOP, Side.WHITE);
-        pieceSide.put(Piece.WHITE_ROOK, Side.WHITE);
-        pieceSide.put(Piece.WHITE_QUEEN, Side.WHITE);
-        pieceSide.put(Piece.WHITE_KING, Side.WHITE);
-
-        pieceSide.put(Piece.BLACK_PAWN, Side.BLACK);
-        pieceSide.put(Piece.BLACK_KNIGHT, Side.BLACK);
-        pieceSide.put(Piece.BLACK_BISHOP, Side.BLACK);
-        pieceSide.put(Piece.BLACK_ROOK, Side.BLACK);
-        pieceSide.put(Piece.BLACK_QUEEN, Side.BLACK);
-        pieceSide.put(Piece.BLACK_KING, Side.BLACK);
-
-
+    Piece(Side side, PieceType type) {
+        this.side = side;
+        this.type = type;
     }
 
     /**
@@ -167,7 +133,7 @@ public enum Piece {
      * @return the piece type
      */
     public PieceType getPieceType() {
-        return pieceType.get(this);
+        return type;
     }
 
     /**
@@ -176,6 +142,6 @@ public enum Piece {
      * @return the piece side
      */
     public Side getPieceSide() {
-        return pieceSide.get(this);
+        return side;
     }
 }
