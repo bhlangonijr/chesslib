948976d03cd48d9b2a034089bf3d68007ef6039f
Alessandro Dal Bello <alessandro.dalbello@gmail.com>
2022-01-06 17:31:33 +0100

    Improve Javadoc of classes and methods in the utils package

--- src/main/java/com/github/bhlangonijr/chesslib/util/XorShiftRandom.java
+++ src/main/java/com/github/bhlangonijr/chesslib/util/XorShiftRandom.java
@@ -1,17 +1,35 @@
 package com.github.bhlangonijr.chesslib.util;
 
+/**
+ * A utility class that is used to generate pseudorandom numbers bases on XOR and bit shifts operations.
+ * <a href="https://en.wikipedia.org/wiki/Xorshift">Xorshift random generators</a> are simple and efficient software
+ * implementations of number generators.
+ */
 public class XorShiftRandom {
 
     private long seed;
 
+    /**
+     * Constructs a new random number generator using a custom long seed.
+     *
+     * @param seed the initial seed
+     */
     public XorShiftRandom(long seed) {
         this.seed = seed;
     }
 
+    /**
+     * Constructs a new random number generator.
+     */
     public XorShiftRandom() {
         this(System.nanoTime());
     }
 
+    /**
+     * Returns the next pseudorandom long value from this random number generator's sequence.
+     *
+     * @return the next pseudorandom long value
+     */
     public long nextLong() {
         seed ^= seed >>> 12;
         seed ^= seed << 25;
